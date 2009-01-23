3968ef2e4ccf3be88e15d7cf0f40c56a3c1e18da
Joel Duffin <joel.duffin@gmail.com>
2009-01-23 15:03:06 -0700

    Added working groups

---
+++ app/controllers/working_groups_controller.rb
@@ -0,0 +1,17 @@
+class WorkingGroupsController < ApplicationController
+  
+  include GroupsHelper
+  include GroupMethods
+  skip_filter :login_required, :only => [:index]
+  
+  def index
+    @visibility_threshold = is_admin? ? -1 : 0
+    @groups = Group.find(:all, :conditions => ['location IS NULL AND visibility > ? AND requires_approval_to_join = true', @visibility_threshold]).paginate(:page => @page, :per_page => @per_page)
+    @results = @groups
+    respond_to do |format|
+      format.html { render :template => 'groups/index'}
+      format.xml  { render :xml => @groups }
+    end
+  end
+  
+end
